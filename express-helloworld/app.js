1986c2918ea02027259fadc4e03966aac804020f
John Doe <johndoe@example.com>
2020-04-18 09:56:24 +0300

    commit

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,14 @@ app.get('/mars', function (req, res) {
   res.send('Hello mars!\n');
 })
 
+app.get('/kars', function (req, res) {
+  res.send('Hello kars!\n');
+})
+
+app.get('/lars', function (req, res) {
+  res.send('Hello lars!\n');
+})
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
